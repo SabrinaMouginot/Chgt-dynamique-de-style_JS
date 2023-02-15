55d557b4870235b402f51e61d82607d08a54c9ea
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-15 02:54:10 +0100

    2 themes

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+var p = document.getElementById('container');
+
+
 function theme_sombre(){
-    var p = document.getElementById('container');
-    p.classList.add("sombre");
+        p;
+        p.classList.add("sombre");
 }
 
 function theme_clair(){
-    var p = document.getElementById('container');
+    p;
     p.classList.add("clair");
 }
+
+// if (onclick="theme_sombre()") {
+//     theme_sombre;
+// } else if (onclick="theme_clair()"){
+//     theme_clair;
+// }else {
+//     theme_clair;
+// }
+
+
+
+// Si le theme cliqué est sombre{
+//     le background est sombre;
+// }sinon{
+//     le background est clair;
+// }
